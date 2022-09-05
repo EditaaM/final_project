0d00b8103a98d8c1d95a1d29af11074ead4f33e3
Edita <e.stundyte@gmail.com>
2022-09-05 22:42:50 +0300

    Created routes

---
+++ 1_frontend/src/App.js
@@ -0,0 +1,18 @@
+import { lazy, Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+const HomePage = lazy(() => import('./pages/HomePage'));
+
+function App() {
+  return (
+    <>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
+          <Route index element={<HomePage />} />
+        </Routes>
+      </Suspense>
+    </>
+  );
+}
+
+export default App;
