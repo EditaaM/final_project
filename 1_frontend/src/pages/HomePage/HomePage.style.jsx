1daecb621c242c1aa0dfcb8649f7801063864ad7
Edita <e.stundyte@gmail.com>
2022-09-09 00:41:37 +0300

    Added style to Home page

---
+++ 1_frontend/src/pages/HomePage/HomePage.style.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const StyledHomePageWrapper = styled.main`
+  display: flex;
+  align-items: baseline;
+  justify-content: center;
+  gap: 100px;
+  margin: 70px auto;
+  max-width: 1280px;
+`;
