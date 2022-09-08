4ad58e6558aa210a9076f97614db70301dab3719
Edita <e.stundyte@gmail.com>
2022-09-09 00:42:59 +0300

    Added Button component

--- 1_frontend/src/components/Table/ReadOnlyRow.js/ReadOnlyRow.jsx
+++ 1_frontend/src/components/Table/ReadOnlyRow.js/ReadOnlyRow.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
+import Button from '../../Button/Button';
 
 const ReadOnlyRow = ({ item, handleEditClick, handleDeleteClick }) => {
   return (
     <tr key={item._id}>
       <td>{item.name}</td>
       <td>{item.email}</td>
-      <td>{item.date}</td>
+      <td>{item.date.slice(0, 10)}</td>
       <td>{item.time}</td>
       <td>
-        <button type="button" onClick={(e) => handleEditClick(e, item)}>
-          Edit
-        </button>
-        <button type="button" onClick={() => handleDeleteClick(item._id)}>
-          Delete
-        </button>
+        <Button
+          text="Edit"
+          bgColor="#7E8BD9"
+          action={(e) => handleEditClick(e, item)}
+          fontColor="white"
+          padding="10px 30px"
+          margin="0 5px 0 0"
+        />
+        <Button
+          action={() => handleDeleteClick(item._id)}
+          text="Delete"
+          bgColor="#3D4A73"
+          fontColor="white"
+          padding="10px 22px"
+          margin="0 0 0 5px"
+        />
       </td>
     </tr>
   );
